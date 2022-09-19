6338a341889528b3fdf4330ba520608b4acc3444
Tiam <tiam@appstudio35.com>
2022-09-18 22:00:25 -0400

    feat:create rest api and retrofit related classes

---
+++ app/src/main/java/com/example/lastfmapp/remote/ApiClient.kt
@@ -0,0 +1,31 @@
+package com.example.lastfmapp.remote
+import com.example.lastfmapp.util.Constants.Companion.API_KEY
+import com.example.lastfmapp.util.Constants.Companion.BASE_URL
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+object ApiClient {
+    private var apiService: ApiService
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        apiService = retrofit.create(ApiService::class.java)
+    }
+    fun getAlbumInfo(
+        artist: String?,
+        album: String?
+    ): Call<ApiResponse> {
+        return apiService.getAlbumInfo(artist, album, API_KEY)
+    }
+    fun searchArtist(
+        artist: String?,
+        album: String?
+    ): Call<ApiResponse> {
+        return apiService.searchArtist(artist, album, API_KEY)
+    }
+
+}
---
+++ app/src/main/java/com/example/lastfmapp/remote/ApiResponse.kt
@@ -0,0 +1,12 @@
+package com.example.lastfmapp.remote
+import com.google.gson.annotations.SerializedName
+
+data class ApiResponse(
+    @SerializedName("artist") val artist: String,
+    @SerializedName("name") val name: String
+//    @SerializedName("image") val images: List<Image>,
+//    @SerializedName("tracks") val tracks: Tracks,
+//    @SerializedName("tags") val tags: TopTags,
+//    @SerializedName("listeners") val listeners: Long,
+//    @SerializedName("playcount") val playCount: Long
+)
