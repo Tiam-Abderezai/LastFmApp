7f5f9eb0154dc48084768ffce36dd8fbb78a5f8e
Tiam <tiam@appstudio35.com>
2022-10-09 12:34:10 -0400

    feat/ implement bottom navigation

--- app/src/main/java/com/example/lastfmapp/albums/AlbumListFragment.kt
+++ app/src/main/java/com/example/lastfmapp/albums/AlbumListFragment.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lastfmapp.databinding.FragmentAlbumListBinding
-import com.example.lastfmapp.util.Log
-import com.example.lastfmapp.util.Log.TAG
-import com.example.lastfmapp.util.Status
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -44,26 +40,26 @@ class AlbumListFragment : Fragment() {
 
     private fun setupLifeCycleObservers() {
         with(albumListViewModel) {
-            getAlbums().observe(viewLifecycleOwner) { topAlbums ->
-                topAlbums?.let { resource ->
-                    when (resource.status) {
-                        Status.SUCCESS -> {
-                            bind.albumListProgressBar.visibility = View.GONE
-                            Log.d(TAG, "SUCCESS ${resource.data}")
-//                             resource.data?.let { movieResponse -> updateUpComingMoviesList(movieResponse = movieResponse) }
-                        }
-                        Status.ERROR -> {
-                            bind.albumListProgressBar.visibility = View.GONE
-                            Log.d(TAG, "ERROR")
-                            Toast.makeText(requireContext(), topAlbums.message, Toast.LENGTH_LONG).show()
-                        }
-                        Status.LOADING -> {
-                            bind.albumListProgressBar.visibility = View.VISIBLE
-                            Log.d(TAG, "LOADING")
-                        }
-                    }
-                }
-            }
+//            getAlbums().observe(viewLifecycleOwner) { topAlbums ->
+//                topAlbums?.let { resource ->
+//                    when (resource.status) {
+//                        Status.SUCCESS -> {
+//                            bind.albumListProgressBar.visibility = View.GONE
+//                            Log.d(TAG, "SUCCESS ${resource.data}")
+// //                             resource.data?.let { movieResponse -> updateUpComingMoviesList(movieResponse = movieResponse) }
+//                        }
+//                        Status.ERROR -> {
+//                            bind.albumListProgressBar.visibility = View.GONE
+//                            Log.d(TAG, "ERROR")
+//                            Toast.makeText(requireContext(), topAlbums.message, Toast.LENGTH_LONG).show()
+//                        }
+//                        Status.LOADING -> {
+//                            bind.albumListProgressBar.visibility = View.VISIBLE
+//                            Log.d(TAG, "LOADING")
+//                        }
+//                    }
+//                }
+//            }
         }
     }
 }
